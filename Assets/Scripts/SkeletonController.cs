bbbdf1c5a5199a1030b4fcd39aa8a1c4fbecc1c4
Saulo Caliman <saulocalimangomes@gmail.com>
2019-05-06 17:11:01 -0300

    Ignorância artificial do esqueleto

--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -6,7 +6,14 @@ using UnityEngine.AI;
 public class SkeletonController : NPC
 {
     Animator meuAnimator;
+    Rigidbody meuCorpo;
     NavMeshAgent meuAgente;
+
+    Player Player;
+
+    public float distanciaDoAlvo;
+
+    public float distanciaMinPlayer;
 
     public List<Transform> Waypoints;
 
@@ -15,12 +22,17 @@ public class SkeletonController : NPC
     public EstadosPersonagem estadoTorso;
     public EstadosPersonagem estadoPernas;
 
+    float velocidadeMax = 12;
+    public int waypointAtual = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         meuAnimator = GetComponent<Animator>();
         meuAgente = GetComponent<NavMeshAgent>();
-        
+        meuCorpo = GetComponent<Rigidbody>();
+
+        Player = GameManager._instance.Player;
     }
 
     // Update is called once per frame
@@ -42,10 +54,64 @@ public class SkeletonController : NPC
             (int)estadoPernas);
     }
 
+    /// <summary>
+    /// Cuida da movimentação da IA do esqueleto
+    /// </summary>
     public void MovimentoAI() {
-        meuAgente.SetDestination(Waypoints[0].position);
+
+        //Velocidade do navmesh agent
+        float velocidadeAtual = meuAgente.velocity.sqrMagnitude;
+
+        //Velocidade entre 0 e 1 para modificar a animação de andar
+        movimentoSpeed = velocidadeAtual / velocidadeMax;
+
+        //Verifica se a movimentação é diferente de 0, se for muda os estados para movimento!
+        //Que são lidos pelo metodo mudar animassaum.
+        if (movimentoSpeed != 0) {
+            estadoPernas = EstadosPersonagem.Movimento;
+            estadoTorso = EstadosPersonagem.Movimento;
+        }
+
+        //Define o destino atual
+        Vector3 destino = Waypoints[waypointAtual].position;
+        
+        //Distancia entre o inimigo e o destino
+        float distancia = Vector3.Distance(transform.position, destino);
+
+        //Distancia do inimigo e o player
+        float distanciaPlayer = Vector3.Distance(transform.position, Player.transform.position);
+
+        //Verifica se a distancia do waypoint é menor que a do alvo
+        //Se for, quer dizer que o inimigo chegou no destino
+        if (distancia <= distanciaDoAlvo) {
+            //Aumenta o waypoint para 1
+            waypointAtual += 1;
+            //Se chegar no último waypoint, começa do começo, ou seja 0
+            if(waypointAtual >= Waypoints.Count){
+                waypointAtual = 0;
+            }
+        }
+
+        //Verifica se o esqueleto está perto do Player
+        //Se não, continua seguindo o waypoint
+        if (distanciaPlayer < distanciaMinPlayer)
+        {
+            //Seta o destino para o player
+            meuAgente.SetDestination(Player.transform.position);
+            //Desenha a linha vermelha
+            Debug.DrawLine(transform.position, Player.transform.position, Color.red);
+        }
+        else {
+            //Seta o destino para o waypoint
+            meuAgente.SetDestination(Waypoints[waypointAtual].position);
+            //Desenha a linha ciana
+            Debug.DrawLine(transform.position, Player.transform.position, Color.cyan);
+        }
     }
 
+    /// <summary>
+    /// Reseta as animações, volta ao estado de movimento
+    /// </summary>
     public void RestearAnimassaum() {
         if (estadoPernas != EstadosPersonagem.Movimento) {
             estadoPernas = EstadosPersonagem.Movimento;
